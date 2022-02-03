19fb3c06fb4b32e96a9811635f910f8a66513478
Barbara113 <barbara.novak999@hotmail.com>
2022-02-03 12:20:25 +0100

    Adding existing components to App.js

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -65,22 +65,27 @@ export default class LoginScreen extends Component {
             </div>
             <label class="signUpWith">Or sign up with:</label>
             <div class="networkIcons">
-              <img
-                src="https://image.similarpng.com/very-thumbnail/2021/01/Facebook-icon-with-flat-design-on-transparent-background-PNG.png"
-                alt="Facebook logo"
-              ></img>
-              <img
-                src="https://www.iconpacks.net/icons/2/free-twitter-logo-icon-2429-thumb.png"
-                alt="Twitter logo"
-              ></img>{" "}
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Gmail_Icon_%282013-2020%29.svg/2048px-Gmail_Icon_%282013-2020%29.svg.png"
-                alt="Gmail logo"
-              ></img>
+              <a href="https://www.facebook.com/" target="_blank">
+                <img
+                  src="https://image.similarpng.com/very-thumbnail/2021/01/Facebook-icon-with-flat-design-on-transparent-background-PNG.png"
+                  alt="Facebook logo"
+                ></img>
+              </a>
+              <a href="https://twitter.com/?lang=hr" target="_blank">
+                <img
+                  src="https://www.iconpacks.net/icons/2/free-twitter-logo-icon-2429-thumb.png"
+                  alt="Twitter logo"
+                ></img>
+              </a>{" "}
+              <a href="https://mail.google.com" target="_blank">
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Gmail_Icon_%282013-2020%29.svg/2048px-Gmail_Icon_%282013-2020%29.svg.png"
+                  alt="Gmail logo"
+                ></img>
+              </a>
             </div>
           </div>
         </div>
-        <div>This is cmtered div</div>
       </>
     );
   }
