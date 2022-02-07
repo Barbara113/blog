0b8d690618706f7043d62fcc9e74e9dd7867038d
Barbara113 <barbara.novak999@hotmail.com>
2022-02-07 15:10:43 +0100

    Cleanup and starting with select

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,35 +3,6 @@ import "./style/input.css";
 import PropTypes from "prop-types";
 
 export default class Input extends Component {
-  static propTypes = {
-    onClick: PropTypes.func,
-    onChange: PropTypes.func,
-    children: PropTypes.node,
-    variant: PropTypes.string,
-    className: PropTypes.string,
-    label: PropTypes.string,
-    size: PropTypes.string,
-    disabledClassName: PropTypes.string,
-    disabled: PropTypes.bool,
-  };
-
-  static defaultProps = {
-    className: "",
-    label: "",
-    size: "",
-    variant: "basic",
-    disabled: false,
-    disabledClassName: "",
-    color: "",
-    border: "",
-    radius: "",
-    height: "",
-    width: "",
-    textColor: "",
-    placeholder: "",
-    value: "",
-    id: "",
-  };
   constructor(props) {
     super(props);
     this.state = {};
@@ -39,7 +10,6 @@ export default class Input extends Component {
 
   render() {
     const {
-      className,
       color,
       border,
       radius,
@@ -50,7 +20,6 @@ export default class Input extends Component {
       onClick,
       placeholder,
       label,
-      value,
       onChange,
       id,
       type,
